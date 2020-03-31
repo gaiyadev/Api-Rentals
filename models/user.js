4d5a477966cb467deac25372274593045954174b
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-03-31 17:03:55 +0100

    Api Auth login done

--- models/user.js
+++ models/user.js
@@ -49,4 +49,10 @@ module.exports.createUser = (newUser, callback) => {
 
 //exports.validate = validateUser;
 
+module.exports.comparePassword = async (password, hash, callback) => {
+    await bcrypt.compare(password, hash, (err, isMatch) => {
+        if (err) throw err;
+        return callback(null, isMatch);
+    });
+}
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
+const Joi = require('joi');
+const _ = require('lodash');
 const User = require('../models/user');
 var express = require('express');
-const Joi = require('joi');
 
 var router = express.Router();
+
 function validateUser(user) {
   const Schema = {
     Name: Joi.string().min(4).max(50).required(),
@@ -24,17 +26,45 @@ router.post('/', async (req, res, next) => {
     if (user) {
       return res.status(400).send('User already exist');
     }
-    const newUser = new User({
-      Name: req.body.Name,
-      Email: req.body.Email,
-      Password: req.body.Password,
-    });
+    const newUser = new User(_.pick(req.body, ['Name', "Email", "Password"]));
     User.createUser(newUser, (err, user) => {
       if (err) throw err;
-      res.send(user);
+      res.send(_.pick(user, ['_id', 'Name', 'Email']));
     });
   });
 
 });
+
+
+function validateLogin(user) {
+  const Schema = {
+    Email: Joi.string().required().email(),
+    Password: Joi.string().min(8).max(50).required(),
+  };
+  return Joi.validate(user, Schema);
+}
+
+/* GET users listing. */
+router.post('/login', async (req, res, next) => {
+  const { error } = validateLogin(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  await User.findOne({ Email: req.body.Email }, (err, user) => {
+    if (err) return err;
+    if (!user) {
+      return res.status(400).send('Invalid Email or Password');
+    }
+    User.comparePassword(req.body.Password, user.Password, (err, isMatch) => {
+      if (err) throw err;
+      if (!isMatch) return res.status(400).send('Invalid Email or Password');
+      return res.status(200).send('login successfully');
+    });
+
+  });
+});
+
+
 
 module.exports = router;
